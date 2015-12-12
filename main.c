f53b50c926a13d91e2f682abb88788d2fa757a0a
swrangsar basumatary <swrangsar@swrangsar.com>
2015-12-12 17:34:08 +0530

    conf_map test: working

--- main.c
+++ main.c
@@ -19,6 +19,16 @@ int main(const int argc, const char **argv)
 	if (found)
 		printf("main: Yay!... found: %s\n", found);
 	
+	rbmap_remove(conf_map, (const void *)"logs_path");
+	
+	if ((found = (char *)rbmap_search(conf_map, (const void *)"logs_path")))
+		printf("main: error: Huh!... found again: %s\n", found);
+	
+	if ((found = (char *)rbmap_search(conf_map, (const void *)"ip_address")))
+		printf("main: found IP: %s\n", found);
+	if ((found = (char *)rbmap_search(conf_map, (const void *)"server_port")))
+		printf("main: found server_port: %s\n", found);
+
 	rbmap_destroy(conf_map);
 	conf_map = NULL;
 	
