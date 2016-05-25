9e6647ff37bd508959a395e856d9c42fc2b6496d
swrangsar basumatary <swrangsar@swrangsar.com>
2016-05-25 19:40:53 +0530

    add c99 debug macros

---
+++ dump/logmsg.c
@@ -0,0 +1,47 @@
+#include "logmsg.h"
+#include <errno.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+
+
+static const char prognm[] = "tlogger";
+static unsigned int verbosity = LOG_LEVEL_DBG;
+
+
+extern int strerror_r(int errnum, char *buf, size_t buflen);
+
+void log_msg(unsigned int level, const char *file, int linenum, const char *fmt, ...)
+{
+	if (level < LOG_LEVEL_CRIT)
+		level = LOG_LEVEL_CRIT;
+	else if (level > LOG_LEVEL_DBG)
+		level = LOG_LEVEL_DBG;
+
+	if (level <= verbosity) {
+		static const char levels[5][6] = {
+			"crit:",
+			"err: ",
+			"warn:",
+			"info:",
+			"dbg: "
+		};
+
+		int errsv = errno;
+		va_list ap;
+
+		fprintf(stderr, "%s: %s %s:%d: ", prognm, levels[level], file, linenum);
+		va_start(ap, fmt);
+		vfprintf(stderr, fmt, ap);
+		va_end(ap);
+
+		if ('\n' != fmt[strlen(fmt)-1]) {
+			char buffer[512];
+
+			strerror_r(errsv, buffer, sizeof(buffer));
+			fprintf(stderr, ":(%d) %s\n", errsv, buffer);
+		}
+
+		fflush(stderr);
+	}
+}
